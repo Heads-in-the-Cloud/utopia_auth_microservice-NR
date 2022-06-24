1e8051e2b89c4a7fd09ae9b92ef056a5be0e8400
nick-sstack <nicholas.ruiz@smoothstack.com>
2022-06-24 14:46:16 -0700

    UTOPIA-425 Changed JSON

--- src/main/java/com/smoothstack/utopia/auth/controller/DeployController.java
+++ src/main/java/com/smoothstack/utopia/auth/controller/DeployController.java
@@ -4,11 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class DeployController {
 
     @GetMapping(path = "/api/auth/deployment")
-    public ResponseEntity<String> health() {
-        return ResponseEntity.ok("Blue");
+    public ResponseEntity<Map<String, String>> health() {
+        Map<String, String> res = new HashMap<>();
+        res.put("Version", "Blue");
+        return ResponseEntity.ok(res);
     }
 }
